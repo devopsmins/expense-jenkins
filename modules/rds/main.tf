c38e7a25d87638616cd480640b2be0d4d35e0888
minhaj <minhajfathima1023@gmail.com>
2024-10-29 06:46:04 -0400

    TF code

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -55,24 +55,8 @@ resource "aws_db_instance" "main" {
         storage_encrypted = true
 }
 
-resource "aws_db_instance" "main" {
-allocated_storage      = var.rds_allocated_storage
-db_name                = "mydb"
-engine                 = var.rds_engine
-engine_version         = var.rds_engine_version
-instance_class         = var.rds_instance_class
-username               = data.aws_ssm_parameter.username.value
-password               = data.aws_ssm_parameter.password.value
-parameter_group_name   = aws_db_parameter_group.main.name
-skip_final_snapshot    = true
-multi_az               = true
-identifier             = "${var.env}-mysql-rds"
-storage_type           = "gp3"
-tags                   = merge(var.tags, { Name = "${var.env}-mysql-rds" })
-db_subnet_group_name   = aws_db_subnet_group.main.name
-vpc_security_group_ids = [aws_security_group.main.id]
-kms_key_id             = var.kms
-storage_encrypted      = true
-}
+
+
+
 
 
