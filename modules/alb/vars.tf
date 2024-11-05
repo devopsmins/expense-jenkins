870b649190d1686c42a0532550fac1fc1ba1c365
pc <minhajfathima1023@gmail.com>
2024-11-05 12:11:04 +0530

    terraform code for app module defining security groups and aws launch template

--- modules/alb/vars.tf
+++ modules/alb/vars.tf
@@ -3,3 +3,9 @@ variable "type" {}
 variable "vpc_id" {}
 variable "sg_cidrs" {}
 variable "tags" {}
+variable "component" {}
+variable "internal" {}
+variable "subnets" {}
+variable "enable_https" {}
+variable "target_group_arn" {}
+variable "certificate_arn" {}
