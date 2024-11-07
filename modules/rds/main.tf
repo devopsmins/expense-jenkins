520f98f0bc8dcea637b041ae239b12a5e45422a5
pc <minhajfathima1023@gmail.com>
2024-11-07 12:46:40 +0530

    terraform code for app module defining security groups and aws launch template

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -17,12 +17,13 @@ resource "aws_security_group" "main" {
 
   ingress {
     description = "MYSQL"
+
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = var.sg_cidrs
   }
-}
+
 
 egress {
   from_port        = 0
@@ -36,27 +37,22 @@ tags = merge(var.tags, { Name = "${var.env}-mysql-rds" })
 }
 
 resource "aws_db_instance" "main" {
-        allocated_storage = var.rds_allocated_storage
-        db_name = "mydb"
-        engine = var.rds_engine
-        engine_version = var.rds_engine_version
-        instance_class = var.rds_instance_class
-        username = data.aws_ssm-parameter.username.value
-        password = data.aws_ssm_parameter.password.value
-        pramaeter_group_name = aws_db_parameter_group.main.name
-        skip_final_snapshot = true
-        multi_az = true
-        identifier = "${var.env}-mysql-rds"
-        storage_type = "gp3"
-        tags = merge(var.tags, { Name = "${var.env}-mysql-rds" })
-        db_subnet_group_name = aws_db_subnet_group.main.name
-        vpc_security_group_ids = [aws_security_group.main.id]
-        kms_key_id = var.kms
-        storage_encrypted = true
+  allocated_storage      = var.rds_allocated_storage
+  db_name                = "mydb"
+  engine                 = var.rds_engine
+  engine_version         = var.rds_engine_version
+  instance_class         = var.rds_instance_class
+  username               = data.aws_ssm_parameter.username.value
+  password               = data.aws_ssm_parameter.password.value
+  parameter_group_name   = aws_db_parameter_group.main.name
+  skip_final_snapshot    = true
+  multi_az               = true
+  identifier             = "${var.env}-mysql-rds"
+  storage_type           = "gp3"
+  tags                   = merge(var.tags, { Name = "${var.env}-mysql-rds" })
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.main.id]
+  kms_key_id             = var.kms
+  storage_encrypted      = true
 }
-
-
-
-
-
 
